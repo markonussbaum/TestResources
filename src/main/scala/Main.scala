14c1846bbecb09d18bb89e24a90fdf6fc7fd7a9d
Marko Nußbaum <marko.nussbaum@maconus.com>
2022-04-05 17:52:23 +0200

    add task information

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 
 import org.apache.spark._
 import org.apache.spark.resource.ResourceInformation
+import org.apache.spark.TaskContext
 
 object Main {
 
@@ -20,6 +21,59 @@ object Main {
 		println("=== RESOURCES ===")
 		println("#resources: " + res.length)
 		res.foreach( println _)
+		println("=================\n\n")
+
+		if (sc.resources.contains("ve")) {
+			val resInfo = sc.resources("ve")
+			println("VE Resource Information:")
+			println("#VEs:      " + resInfo.addresses.length)
+			println("name:      " + resInfo.name)
+			println("addresses: ")
+			resInfo.addresses.foreach(adr => println("  #" + adr))
+		} else {
+			println("No VE resources have been assigned to the driver.")
+		}
+
+		// get info from executors
+		//val driverHost = sc.getConf("spark.driver.host")
+
+		val rdd = sc.parallelize(Array(1,2,3,4,5,6,7,8))
+		val result = rdd.map ( number => {
+
+			// we are now on an executor, collect information
+			val ctx = TaskContext.get()
+
+			// build a string with info from executor
+			var sb = new StringBuilder
+			sb.append("Mapping number " + number + "\n==============\n\n")
+			sb.append(
+				"Task on partition with id " + ctx.partitionId()
+					+ " on stage with id " + ctx.stageId() + "\n"
+					+ "at attempt no. " + + ctx.attemptNumber()
+					+ " with task attempt id: " + ctx.taskAttemptId() + "\n"
+			)
+
+			if (ctx.resources.contains("ve")) {
+				sb.append(ctx.resources.get("ve") match {
+					case Some(resourceInformation: ResourceInformation) => {
+						("was assigned VE(s) at address(es):\n"
+						+ resourceInformation.addresses.map( s => "  #"+s).reduceLeft(_+"\n"+_)
+						+ "\n")
+					}
+					case None => {
+						"was not assigned any VE"
+					}
+				})
+			} else {
+				sb.append("executor at does not contain any VEs in resources.")
+			}
+			sb.toString
+		}).reduce(_ + "\n" + _)
+
+		println("\nInfo from executors:\n\n")
+		println(result)
+
+		sc.stop()
 	}
 
 	def setup(): SparkContext = {
